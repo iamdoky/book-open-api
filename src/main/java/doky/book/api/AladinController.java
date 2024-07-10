72b84555cb278a1017588e268253d7ee6c37249e
iamdoky <kdh6631@gmail.com>
2024-07-10 22:33:36 +0900

    feat : add aladin controller

---
+++ src/main/java/doky/book/api/AladinController.java
@@ -0,0 +1,15 @@
+package doky.book.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "알라딘")
+@RequestMapping("/api/aladin")
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class AladinController {
+}
